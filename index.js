82e491d37dcca6426c249661c2d12bd0460d2d52
Przemysław Afeltowicz <przemyslaw.afeltowicz@gmail.com>
2017-06-18 23:11:09 +0200

    Install handlebars

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-let express = require('express');
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
 
-let app = express();
+const routes = require('./routes/index');
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
+const app = express();
+
+// view engine setup
+app.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/' }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.listen(3000, () => {
-    console.log('Server started port: 3000');
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
+
+app.listen('3000', () => {
+    console.log('Start server');
+});
+
+module.exports = app;
